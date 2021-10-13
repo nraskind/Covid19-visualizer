460782a3ca92cc420bcb2595634297e16e1d5d13
nraskind <nraskind@umich.edu>
2021-10-13 00:23:02 -0400

    Uncommented shinyApp to test interface. Now succesfully runs the app.

--- app.R
+++ app.R
@@ -4,7 +4,6 @@ rm(list=ls()) # clear all environmental variables
 if (!require(RCurl)) install.packages("RCurl") # for webscraping
 if (!require(dplyr)) install.packages("dplyr") # for extracting/filtering
 if(!require(shiny)) install.packages("shiny") # for webpage
-
 # consider putting in a helper function script
 get_csv <- function(download_url) {
   # reads in a csv from an html-formatted download link
@@ -12,7 +11,9 @@ get_csv <- function(download_url) {
   return(read.csv(text=readable_url))
 }
 
-# All data was retrieved from the [CDC Official Website](https://www.cdc.gov/nchs/nvss/vsrr/covid_weekly/)
+# import ui and server
+
+# update data tables
 sex_and_age <- get_csv("https://data.cdc.gov/api/views/9bhg-hcku/rows.csv?accessType=DOWNLOAD")
 week_sex_and_age <- get_csv("https://data.cdc.gov/api/views/vsak-wrfu/rows.csv?accessType=DOWNLOAD")
 race_and_hispanic_origin <- get_csv("https://data.cdc.gov/api/views/pj7m-y5uh/rows.csv?accessType=DOWNLOAD")
@@ -22,5 +23,6 @@ comorbities_and_conditions <- get_csv("https://data.cdc.gov/api/views/hk9y-quqm/
 deaths_by_county <- get_csv("https://data.cdc.gov/api/views/kn79-hsxy/rows.csv?accessType=DOWNLOAD")
 deaths_by_week_and_urbanicity <- get_csv("https://data.cdc.gov/api/views/hkhc-f7hg/rows.csv?accessType=DOWNLOAD")
 
+
 ### RUN APP ###
-# shinyApp(ui, server)
+shinyApp(ui, server)
