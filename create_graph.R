d47e9b5063adade1c69d397fde0b841b0abbf6de
nraskind <nraskind@umich.edu>
2021-12-06 16:32:49 -0500

    configured to work with the combined dataframe

--- create_graph.R
+++ create_graph.R
@@ -21,19 +21,20 @@ label = list(
   font = font
 )
 
-covid_graph <- plot_geo(test,
-                locationmode="USA-states",
-                frame = ~month_num
-                ) %>%
-  add_trace(locations = ~code,
-  z = ~covid_deaths, # z will be whatever they want to measure against
-  zmin = 0,
-  color = ~covid_deaths,
-  text = ~hover,
-  hoverinfo = 'text') %>%
-  layout(geo = graph_properties,
-         title="Pandemic Statistics in the US (By Month)"
+covid_graph <- plot_geo(combined_dataframe,
+                        locationmode = "USA-states",
+                        frame = ~ month_num) %>%
+  add_trace(
+    locations = ~ code,
+    z = ~ covid_deaths,
+    # z will be whatever they want to measure against, same with color
+    zmin = 0,
+    color = ~ covid_deaths,
+    text = ~ hover,
+    hoverinfo = 'text'
   ) %>%
+  layout(geo = graph_properties,
+         title = "Pandemic Statistics in the US (By Month)") %>%
   config(displayModeBar = FALSE) %>%
   style(hoverlabel = label)
 
