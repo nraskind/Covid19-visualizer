72ee823e33f156c74b2a8db4faac290eab996396
nraskind <nraskind@umich.edu>
2021-12-06 16:33:19 -0500

    Added male and female covid data

--- create_combined_dataframe.R
+++ create_combined_dataframe.R
@@ -42,21 +42,20 @@ total_deaths <- filter(sa_by_month, Age.Group=="All Ages")["Total.Deaths"]
 covid_deaths <- filter(sa_by_month, Age.Group=="All Ages")["COVID.19.Deaths"]
 
 ### AGE STATS ###
-adolescents <- filter(sa_by_month,
-                      Age.Group == "0-17 years")["COVID.19.Deaths"]
+adolescents <- filter(sa_by_month, Age.Group == "0-17 years")["COVID.19.Deaths"]
 
-adults <- filter(
-  sa_by_month,
-  Age.Group %in% c("18-29 years",
-                   "30-39 years",
-                   "40-49 years",
-                   "50-64 years")
-)["COVID.19.Deaths"]
+adults <- filter(sa_by_month,
+                 Age.Group %in% c("18-29 years",
+                                  "30-39 years",
+                                  "40-49 years",
+                                  "50-64 years"))["COVID.19.Deaths"] %>%
+  rowsum(rep (1:(nrow(.) / 4), each = 4))
 
 seniors <- filter(sa_by_month,
                   Age.Group %in% c("65-74 years",
                                    "75-84 years",
-                                   "85 years and over"))["COVID.19.Deaths"]
+                                   "85 years and over"))["COVID.19.Deaths"] %>%
+  rowsum(rep(1:(nrow(.) / 3), each = 3))
 
 ### RACE STATS ###
 race_by_month <- filter(race_and_hispanic_origin,
@@ -76,25 +75,36 @@ other <- rowSums(
                         ),
                  na.rm=TRUE)
 
-adults <- rowsum(adults, rep( 1:(nrow(adults)/4), each=4))
-seniors <- rowsum(seniors, rep( 1:(nrow(seniors)/3), each=3))
+### SEX STATS ###
+male <- filter(sex_and_age,
+               Group=="By Month",
+               Age.Group=="All Ages",
+               Sex=="Male",
+               !(State %in% c("Puerto Rico", "New York City", "United States")))["COVID.19.Deaths"]
+female <- filter(sex_and_age,
+               Group=="By Month",
+               Age.Group=="All Ages",
+               Sex=="Female",
+               !(State %in% c("Puerto Rico", "New York City", "United States")))["COVID.19.Deaths"]
 
 combined_dataframe <- data.frame(
-  "year"=meta["Year"],
-  "month"=meta["Month"],
-  "state"=meta["State"],
-  "month_num"=meta["month_num"],
-  "code"=meta["code"],
-  "adolescents"=adolescents,
-  "adults"=adults,
-  "seniors"=seniors,
-  "total_deaths"=total_deaths,
-  "covid_deaths"=covid_deaths,
-  "white"=white,
-  "black"=black,
-  "hispanic"=hispanic,
-  "asian"=asian,
-  "other race"=other
+  "year" = meta["Year"],
+  "month" = meta["Month"],
+  "state" = meta["State"],
+  "month_num" = meta["month_num"],
+  "code" = meta["code"],
+  "male"=male,
+  "female"=female,
+  "adolescents" = adolescents,
+  "adults" = adults,
+  "seniors" = seniors,
+  "total_deaths" = total_deaths,
+  "covid_deaths" = covid_deaths,
+  "white" = white,
+  "black" = black,
+  "hispanic" = hispanic,
+  "asian" = asian,
+  "other race" = other
 )
 
 colnames(combined_dataframe) <- c("year",
@@ -102,6 +112,8 @@ colnames(combined_dataframe) <- c("year",
                                   "state",
                                   "month_num",
                                   "code",
+                                  "male",
+                                  "female",
                                   "adolescents",
                                   "adults",
                                   "seniors",
@@ -113,10 +125,8 @@ colnames(combined_dataframe) <- c("year",
                                   "asian",
                                   "other race")
 
-
 # replace NAs with 0s
 combined_dataframe[is.na(combined_dataframe)] <- 0
-
 
 combined_dataframe <- mutate(combined_dataframe, 
                              hover=paste0("Total Number of Deaths: ", total_deaths,
